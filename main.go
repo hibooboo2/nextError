0323853af6268152e95361949791008705728fea
James Jeffrey <james.jeffrey@acronis.com>
2022-05-24 12:39:24 -0700

    Now it will move to new errors as they form hopefully

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"bytes"
 	"flag"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 	"time"
@@ -34,34 +36,29 @@ func main() {
 	closeOnNoError := flag.Bool("e", false, "close when no errors")
 	useShortCuts := flag.Bool("h", false, "hotkey for next error")
 	shouldLog := flag.Bool("v", false, "Verbose log events")
+	shouldLogOnErrorFix := flag.Bool("logonfix", false, "Log on error fixed")
+
 	flag.Parse()
 	if !*shouldLog {
 		log.SetOutput(ioutil.Discard)
 	}
 	w, err := fsnotify.NewWatcher()
-	defer w.Close()
 	if err != nil {
 		panic(err)
 	}
+
+	defer func() { _ = w.Close() }()
+
 	move := make(chan struct{}, 2)
 
 	errs := GetListOfErrors()
+	currentLocation, pos := GetFirstError(errs, w, *closeOnNoError)
 
-	var currentLocation *BuildError
-	pos := 0
-	if len(errs) > 0 {
-		currentLocation = errs[0]
-		pos = 0
-		currentLocation.Open()
-		err = w.Add(currentLocation.File)
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		if *closeOnNoError {
-			return
-		}
+	for currentLocation == nil {
+		errs = GetListOfErrors()
+		currentLocation, pos = GetFirstError(errs, w, *closeOnNoError)
 	}
+
 	log.Println(pos)
 	go func() {
 		for range w.Events {
@@ -69,6 +66,7 @@ func main() {
 			move <- struct{}{}
 		}
 	}()
+
 	if *useShortCuts {
 		go shortCuts(errs, pos)
 		defer robotgo.End()
@@ -85,6 +83,9 @@ func main() {
 			continue
 		}
 		if len(errs) > 0 && errs[0].Location() != currentLocation.Location() {
+			if *shouldLogOnErrorFix {
+				fmt.Println("Fixed Error:", currentLocation.Location())
+			}
 			w.Remove(currentLocation.File)
 			currentLocation = errs[0]
 			pos = 0
@@ -96,6 +97,24 @@ func main() {
 
 		}
 	}
+}
+
+func GetFirstError(errs []*BuildError, w *fsnotify.Watcher, closeOnNoError bool) (*BuildError, int) {
+	if len(errs) > 0 {
+		currentLocation := errs[0]
+		pos := 0
+		currentLocation.Open()
+		err := w.Add(currentLocation.File)
+		if err != nil {
+			panic(err)
+		}
+		return currentLocation, pos
+	} else {
+		if closeOnNoError {
+			os.Exit(0)
+		}
+	}
+	return nil, 0
 }
 
 func GetListOfErrors() []*BuildError {
